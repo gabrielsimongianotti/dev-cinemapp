d0470696b87d2d08148690fe68e2a48bca6c81bb
gabrielsimongianotti <gabrielgianotti5@gmail.com>
2021-02-24 05:09:20 -0300

    add CardList that is a FlatList

--- app/src/pages/favoritos/styled.ts
+++ app/src/pages/favoritos/styled.ts
@@ -1,8 +1,10 @@
 import styled from 'styled-components/native'
 import { Platform } from 'react-native';
+import { FlatList } from 'react-native';
+import { Movie } from './index';
 
-export const ScrollView = styled.ScrollView`
-  min-height:100%
+export const CardList = styled(FlatList as new () => FlatList<Movie>)`
+  min-height: 115%;
 `;
 
 export const View = styled.View`
@@ -10,6 +12,7 @@ export const View = styled.View`
 `;
 
 export const Container = styled.View`
+  margin-top:10px;
   padding: 0 30px ${Platform.OS === "android" ? 110 : 40}px;
   flex:1
 `;
